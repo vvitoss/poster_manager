5e516afbeeedab068ac215295d3b5546e0deaa13
vvitoss <orangekaramel@gmail.com>
2022-02-16 20:18:24 +0300

    bugs fixed in fail operations

--- src/main/java/ru/netology/poster_manager/Operations.java
+++ src/main/java/ru/netology/poster_manager/Operations.java
@@ -3,22 +3,23 @@ package ru.netology.poster_manager;
 public class Operations {
     private PosterManager[] items = new PosterManager[0];
 
+
     public void save(PosterManager item) {
         int length = items.length + 1;
         PosterManager[] tmp = new PosterManager[length];
         for (int i = 0; i < items.length; i++) {
             tmp[i] = items[i];
         }
-        System.arraycopy(items, 0, tmp, 0, items.length);
+       // System.arraycopy(items, 0, tmp, 0, items.length);
         int lastIndex = tmp.length - 1;
         tmp[lastIndex] = item;
         items = tmp;
     }
 
     public PosterManager[] findAll() {
-        int[] film = {1, 2, 3, 4, 5, 6, 7, 8, 9, 10};
-        for (int i = film.length - 1; i <= 0; i--) {
-        }
+      //  int[] film = {1, 2, 3, 4, 5, 6, 7, 8, 9, 10};
+      //  for (int i = film.length - 1; i <= 0; i--) {
+     //   }
         return items;
     }
 
@@ -27,7 +28,8 @@ public class Operations {
         int lastIndex = 10;
         if (resultLength > lastIndex) {
             resultLength = lastIndex;
-        } else resultLength = items.length;
+        } else { resultLength = items.length;
+        }
         PosterManager[] result = new PosterManager[items.length];
         for (int i = 0; i < resultLength; i++) {
             int index = items.length - i - 1;
--- src/main/java/ru/netology/poster_manager/PosterManager.java
+++ src/main/java/ru/netology/poster_manager/PosterManager.java
@@ -10,9 +10,58 @@ import lombok.NoArgsConstructor;
 @Data
 
 public class PosterManager {
-    public int length;
-    private int id;
+    public int id;
+    private int productId;
     private String filmName;
     private String filmGenre;
     private int ticketPrice;
+
+   // public int getId() {
+    //    return id;
+  //  }
+
+  //  public void setId(int id) {
+   //     this.id = id;
+   // }
+
+   // public int getProductId() {
+   //     return productId;
+  //  }
+
+  //  public void setProductId(int productId) {
+   //     this.productId = productId;
+   // }
+
+   // public String getFilmName() {
+     //   return filmName;
+   // }
+
+  //  public void setFilmName(String filmName) {
+   //     this.filmName = filmName;
+  //  }
+
+  //  public String getFilmGenre() {
+   //     return filmGenre;
+  //  }
+
+  //  public void setFilmGenre(String filmGenre) {
+   //     this.filmGenre = filmGenre;
+  //  }
+
+ //   public int getTicketPrice() {
+    //    return ticketPrice;
+ //   }
+
+   // public void setTicketPrice(int ticketPrice) {
+   //     this.ticketPrice = ticketPrice;
+   // }
+
+  //  public PosterManager(int id, int productId, String filmName, String filmGenre, int ticketPrice) {
+     //   this.id = id;
+    //    this.productId = productId;
+     //   this.filmName = filmName;
+     //   this.filmGenre = filmGenre;
+     //   this.ticketPrice = ticketPrice;
+ //   }
 }
+
